766da7764dc6e60a2472b283f9936942e54e89d3
Gustavo Neiva <luizgzn@gmail.com>
2023-02-03 15:24:29 +0800

    add header and initial loading

---
+++ src/components/Header.svelte
@@ -0,0 +1,48 @@
+<script>
+</script>
+
+<header>
+	<div class="menu-left">menu left</div>
+	<div class="title">
+		<h1>Canine Challenge</h1>
+	</div>
+	<div class="menu-right">menu right</div>
+</header>
+
+<style lang="postcss">
+	header {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		flex-wrap: nowrap;
+		height: 6rem;
+		border-bottom: 1px solid rgb(159, 159, 159);
+		margin-bottom: 0.5rem;
+		padding: 0 1rem;
+	}
+
+	.title {
+		flex-grow: 2;
+		font-weight: 700;
+		font-size: 2rem;
+		letter-spacing: 0.01em;
+		text-align: center;
+		left: 0;
+		right: 0;
+		pointer-events: none;
+		position: relative;
+	}
+
+	.menu-left {
+		display: flex;
+		margin: 0;
+		padding: 0;
+		align-items: center;
+		justify-content: flex-start;
+	}
+
+	.menu-right {
+		display: flex;
+		justify-content: flex-end;
+	}
+</style>
---
+++ src/components/InitialLoading.svelte
@@ -0,0 +1,38 @@
+<script>
+	import LottiePlayer from '$components/LottiePlayer.svelte';
+	$: innerHeight = 0;
+	$: innerWidth = 0;
+</script>
+
+<svelte:window bind:innerWidth bind:innerHeight />
+
+<div class="loading">
+	<p>Woofing Up Your Knowledge!</p>
+	<LottiePlayer path={'./dog-walking.json'} height={innerWidth / 3} width={innerWidth / 3} />
+</div>
+
+<style lang="postcss">
+	.loading {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	p {
+		font-size: 3rem;
+		animation: flickerAnimation 2s infinite;
+	}
+
+	@keyframes flickerAnimation {
+		0% {
+			opacity: 1;
+		}
+		50% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+</style>
