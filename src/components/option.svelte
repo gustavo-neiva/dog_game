0ba278c97659795417cffc24085655da8cdd1454
Gustavo Neiva <luizgzn@gmail.com>
2023-02-11 22:07:17 +0800

    add animations to question and option

--- src/components/option.svelte
+++ src/components/option.svelte
@@ -1,70 +1,96 @@
 <script>
-	export let breed, correct, answered, disabled;
-	$: answeredClassName = correct ? 'correct' : 'wrong';
-	$: answeredClass = answered ? answeredClassName : '';
-	$: paintRight = disabled && correct ? 'correct' : '';
+  export let breed, correct, answered, disabled;
+  $: answeredClassName = correct ? "correct" : "wrong";
+  $: answeredClass = answered ? answeredClassName : "";
+  $: paintRight = disabled && correct ? "correct" : "";
 </script>
 
 <div class="option {answeredClass} {paintRight} " on:click class:disabled>
-	<span class="text">
-		{breed}
-	</span>
+  <span class="text">
+    {breed}
+  </span>
 </div>
 
 <style lang="postcss">
-	.option {
-		position: relative;
-		cursor: pointer;
-		background-color: #fafaff;
-		border-radius: 3.3rem;
-		transition: all 0.2s ease-in-out;
-		padding: 0.675rem 0;
-		color: #37383a;
-		width: 80%;
-		max-width: 60rem;
-		height: auto;
+  .option {
+    position: relative;
+    cursor: pointer;
+    background-color: #fafaff;
+    border-radius: 3.3rem;
+    transition: all 0.2s ease-in-out;
+    padding: 0.675rem 0;
+    color: #37383a;
+    width: 80%;
+    max-width: 60rem;
+    height: auto;
 
-		@media (max-width: 768px) {
-			&:last-child {
-				margin-bottom: 3rem;
-			}
-		}
-		@media (min-width: 1024px) {
-			font-size: 2.8rem;
-			margin: 1.2rem 0;
-		}
+    @media (max-width: 768px) {
+      &:last-child {
+        margin-bottom: 3rem;
+      }
+    }
+    @media (min-width: 1024px) {
+      font-size: 2.8rem;
+      margin: 1.2rem 0;
+    }
 
-		@media (min-width: 768px) and (max-width: 1024px) {
-			font-size: 2.6rem;
-			margin: 1rem 0;
-		}
+    @media (min-width: 768px) and (max-width: 1024px) {
+      font-size: 2.6rem;
+      margin: 1rem 0;
+    }
 
-		@media (max-width: 768px) {
-			font-size: 2.4rem;
-		}
+    @media (max-width: 768px) {
+      margin: 0.5rem 0;
+      font-size: 2rem;
+    }
 
-		&:last-child {
-			margin-bottom: 3.3rem;
-		}
+    &:last-child {
+      margin-bottom: 3.3rem;
+    }
 
-		&:hover {
-			transform: scale(101.3%);
-		}
-	}
+    &:hover {
+      transform: scale(101.3%);
+    }
+  }
 
-	.correct {
-		background-color: rgb(89, 195, 100);
-	}
+  .correct {
+    background-color: rgb(89, 195, 100);
+  }
 
-	.wrong {
-		background-color: rgb(200, 45, 81);
-	}
+  .wrong {
+    background-color: rgb(200, 45, 81);
+    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+    transform: translate3d(0, 0, 0);
+  }
 
-	.disabled {
-		pointer-events: none;
-	}
+  .disabled {
+    pointer-events: none;
+  }
 
-	.text {
-		padding-left: 1rem;
-	}
+  .text {
+    padding-left: 1rem;
+  }
+
+  @keyframes shake {
+    10%,
+    90% {
+      transform: translate3d(-1px, 0, 0);
+    }
+
+    20%,
+    80% {
+      transform: translate3d(2px, 0, 0);
+    }
+
+    30%,
+    50%,
+    70% {
+      transform: translate3d(-4px, 0, 0);
+    }
+
+    40%,
+    60% {
+      transform: translate3d(4px, 0, 0);
+    }
+  }
 </style>
--- src/components/question.svelte
+++ src/components/question.svelte
@@ -1,188 +1,193 @@
 <script>
-	import { updateGame } from '../lib/repository';
-	import { fade, fly } from 'svelte/transition';
-	import { cubicOut } from 'svelte/easing';
-	import DogImg from './DogImg.svelte';
-	import Option from './Option.svelte';
-	import Arrow from './Arrow.svelte';
-	import LottiePlayer from './LottiePlayer.svelte';
-	import {
-		quizIndex,
-		answers,
-		answerIndex,
-		back,
-		next,
-		durationIn,
-		durationOut,
-		xIn
-	} from '../store';
+  import { updateGame } from "../lib/repository";
+  import { fade } from "svelte/transition";
+  import DogImg from "./DogImg.svelte";
+  import Option from "./Option.svelte";
+  import Arrow from "./Arrow.svelte";
+  import LottiePlayer from "./LottiePlayer.svelte";
+  import {
+    quizIndex,
+    answers,
+    answerIndex,
+    back,
+    next,
+    durationIn,
+    quiz,
+  } from "../store";
 
-	export let index;
-	export let image;
-	export let options;
-	export let answered;
+  export let index;
+  export let image;
+  export let options;
+  export let answered;
 
-	let xDown;
-	let animationHeight = innerHeight;
-	let animate = false;
+  let xDown;
+  let animationHeight = innerHeight;
+  let animate = false;
 
-	$: hasAnswered = answered;
-	$: selectedOption = { breed: null, correct: null };
-	$: innerWidth = 0;
-	$: innerHeight = 0;
-	$: isCorrect = null;
-	$: positionY = 0;
+  $: hasAnswered = answered;
+  $: selectedOption = { breed: null, correct: null };
+  $: innerWidth = 0;
+  $: innerHeight = 0;
+  $: isCorrect = null;
+  $: positionY = 0;
 
-	if (innerWidth > 760) {
-		animationHeight = animationHeight * 1.1;
-	}
+  if (innerWidth > 760) {
+    animationHeight = animationHeight * 1.1;
+  }
 
-	const answer = (e) => {
-		hasAnswered = true;
-		isCorrect = selectedOption.correct;
-		$answers = [...$answers, selectedOption];
-		const button = e.target;
-		const rect = button.getBoundingClientRect();
-		positionY = rect.top;
-		answerIndex.update((n) => n + 1);
-		if (isCorrect) {
-			animate = true;
-		}
-		updateGame();
-	};
+  const answer = (e) => {
+    hasAnswered = true;
+    isCorrect = selectedOption.correct;
+    $answers = [...$answers, selectedOption];
+    quiz.update((items) => {
+      const item = items[$quizIndex];
+      item.answered = true;
+      items[$quizIndex] = item;
+      const newItems = [...items];
+      return newItems;
+    });
+    const button = e.target;
+    const rect = button.getBoundingClientRect();
+    positionY = rect.top;
+    answerIndex.update((n) => n + 1);
+    updateGame();
+    if (isCorrect) {
+      animate = true;
+    }
+  };
 
-	const handleTouchMove = (e) => {
-		if (!xDown) {
-			return;
-		}
+  const handleTouchMove = (e) => {
+    if (!xDown) {
+      return;
+    }
 
-		let xUp = e.touches[0].clientX;
+    let xUp = e.touches[0].clientX;
 
-		let xDiff = xDown - xUp;
+    let xDiff = xDown - xUp;
 
-		if (xDiff > 0) {
-			if (hasAnswered) {
-				next();
-			}
-		} else {
-			if ($quizIndex >= 1) {
-				back();
-			}
-		}
+    if (xDiff > 0) {
+      if (hasAnswered) {
+        next();
+      }
+    } else {
+      if ($quizIndex >= 1) {
+        back();
+      }
+    }
 
-		xDown = null;
-	};
+    xDown = null;
+  };
 
-	const handleTouchStart = (e) => {
-		const firstTouch = e.touches[0];
-		xDown = firstTouch.clientX;
-	};
+  const handleTouchStart = (e) => {
+    const firstTouch = e.touches[0];
+    xDown = firstTouch.clientX;
+  };
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
 {#if $quizIndex === index}
-	{#if $quizIndex >= 1}
-		<div in:fade={{ duration: $durationIn }} out:fade>
-			<Arrow direction="left" />
-		</div>
-	{/if}
+  {#if $quizIndex >= 1}
+    <div in:fade={{ duration: $durationIn }}>
+      <Arrow direction="left" />
+    </div>
+  {/if}
 
-	{#if hasAnswered}
-		<div in:fade={{ duration: $durationIn }} out:fade>
-			<Arrow direction="right" />
-		</div>
-	{/if}
+  {#if hasAnswered}
+    <div in:fade={{ duration: $durationIn }}>
+      <Arrow direction="right" />
+    </div>
+  {/if}
 
-	<div
-		class="question"
-		in:fly|local={{ x: $xIn, duration: $durationIn, easing: cubicOut }}
-		out:fade={{ duration: $durationOut }}
-		on:touchmove={handleTouchMove}
-		on:touchstart={handleTouchStart}
-	>
-		<div class="image">
-			<DogImg {image} />
-		</div>
-		<div class="center">
-			<h3>What breed is this dog?</h3>
-		</div>
-		{#each options as option}
-			<Option
-				{...option}
-				on:click={(e) => {
-					selectedOption = option;
-					answer(e);
-				}}
-				answered={selectedOption.breed === option.breed && hasAnswered}
-				disabled={hasAnswered}
-			/>
-		{/each}
-	</div>
-	{#if hasAnswered && isCorrect && animate}
-		<div class="animation" style="top: {positionY}px;">
-			<LottiePlayer
-				path={'./correct.json'}
-				height={animationHeight}
-				width={innerWidth}
-				loop={false}
-				on:finish={() => (animate = false)}
-			/>
-		</div>
-	{/if}
+  <div
+    class="question"
+    in:fade={{ duration: $durationIn }}
+    on:touchmove={handleTouchMove}
+    on:touchstart={handleTouchStart}
+  >
+    <div class="image">
+      <DogImg {image} />
+    </div>
+    <div class="center">
+      <h3>What breed is this dog?</h3>
+    </div>
+    {#each options as option}
+      <Option
+        {...option}
+        on:click={(e) => {
+          selectedOption = option;
+          answer(e);
+        }}
+        answered={selectedOption.breed === option.breed && hasAnswered}
+        disabled={hasAnswered}
+      />
+    {/each}
+  </div>
+  {#if hasAnswered && isCorrect && animate}
+    <div class="animation" style="top: {positionY}px;">
+      <LottiePlayer
+        path={"./correct.json"}
+        height={animationHeight}
+        width={innerWidth}
+        loop={false}
+        on:finish={() => (animate = false)}
+      />
+    </div>
+  {/if}
 {/if}
 
 <style lang="postcss">
-	h3 {
-		text-align: center;
-		color: darkgray;
+  h3 {
+    text-align: center;
+    color: darkgray;
 
-		@media screen and (min-width: 1024px) {
-			font-size: 3.6rem;
-		}
+    @media screen and (min-width: 1024px) {
+      font-size: 3.6rem;
+    }
 
-		@media screen and (min-width: 768px) and (max-width: 1024px) {
-			font-size: 3.4rem;
-		}
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+      font-size: 3.4rem;
+    }
 
-		@media screen and (max-width: 768px) {
-			font-size: 2.6rem;
-		}
-	}
+    @media screen and (max-width: 768px) {
+      font-size: 2.6rem;
+    }
+  }
 
-	.image {
-		margin: 0 auto;
-		display: block;
+  .image {
+    margin: 0 auto;
+    display: block;
 
-		@media screen and (min-width: 1024px) {
-			margin: 1.5rem;
-			height: 35rem;
-			width: 35rem;
-		}
+    @media screen and (min-width: 1024px) {
+      margin: 1.5rem;
+      height: 35rem;
+      width: 35rem;
+    }
 
-		@media screen and (min-width: 768px) and (max-width: 1024px) {
-			margin: 1rem;
-			height: 32rem;
-			width: 32rem;
-		}
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+      margin: 1rem;
+      height: 32rem;
+      width: 32rem;
+    }
 
-		@media screen and (max-width: 768px) {
-			margin: 0.5rem;
-			height: 29rem;
-			width: 29rem;
-		}
-	}
-	.question {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-evenly;
-		align-items: center;
-	}
+    @media screen and (max-width: 768px) {
+      margin: 0.5rem;
+      height: 25rem;
+      width: 25rem;
+    }
+  }
+  .question {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 100%;
+  }
 
-	.animation {
-		position: absolute;
-		z-index: 10;
-		transform: translate(-50%, -50%);
-		left: 50%;
-	}
+  .animation {
+    position: absolute;
+    z-index: 10;
+    transform: translate(-50%, -50%);
+    left: 50%;
+  }
 </style>
